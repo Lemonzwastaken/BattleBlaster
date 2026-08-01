73ede234d3e53313e6d80dbfe47ecf1555296500
Aaradhy Srivastava <aaradhyarya.srivastava@gmail.com>
2026-08-01 18:23:23 +0530

    Setup Base Pawn to base Turrets and Tanks on

---
+++ Source/BattleBlaster/BasePawn.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BasePawn.h"
+
+// Sets default values
+ABasePawn::ABasePawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+
+	//Setup Capsule Component, Base and Turret mesh
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComp"));
+	SetRootComponent(CapsuleComp);
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Component"));
+	CapsuleComp->SetupAttachment(BaseMesh);
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Component"));
+	CapsuleComp->SetupAttachment(TurretMesh);
+
+
+}
+
+// Called when the game starts or when spawned
+void ABasePawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABasePawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABasePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
