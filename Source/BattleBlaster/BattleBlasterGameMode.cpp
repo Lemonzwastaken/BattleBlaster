e7e37b0f1f0cbf86163b425b109f5dfda76371cc
Aaradhy Srivastava <aaradhyarya.srivastava@gmail.com>
2026-08-02 05:51:39 +0530

    Setting tank location and pointer in turrets

--- Source/BattleBlaster/BattleBlasterGameMode.cpp
+++ Source/BattleBlaster/BattleBlasterGameMode.cpp
@@ -2,4 +2,47 @@
 
 
 #include "BattleBlasterGameMode.h"
+#include "Kismet/GameplayStatics.h"
+#include "Tower.h"
 
+void ABattleBlasterGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TArray<AActor*> Towers;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ATower::StaticClass(), Towers);
+
+	TowerCount = Towers.Num();
+
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+	if (PlayerPawn)
+	{
+		Tank = Cast<ATank>(PlayerPawn);
+		
+		if (Tank == nullptr)
+		{
+			UE_LOG(LogTemp, Display, TEXT("Game unable to find tonk"));
+		}
+
+	}
+	int32 LoopIndex = 0;
+
+	while (LoopIndex < TowerCount)
+	{
+		AActor* TowerActor = Towers[LoopIndex];
+		if (TowerActor)
+		{
+			ATower* Tower = Cast<ATower>(TowerActor);
+			if (Tower&& Tank)
+			{
+				Tower->Tank = Tank;
+			}
+		}
+
+		LoopIndex++;
+	}
+
+
+
+}
